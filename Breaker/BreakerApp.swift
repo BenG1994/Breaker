728db5a8ac018534dae7fc329622132b13c36c68
BenG1994 <113778995+BenG1994@users.noreply.github.com>
2023-04-01 14:14:30 -0500

    Initial Commit

---
+++ Breaker/BreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakerApp.swift
+//  Breaker
+//
+//  Created by Ben Gauger on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
